d7c1570ffee11ec3fba0a60b9aecd483b07591a4
Sourya Kakarla <sourya4@ladduu.com>
2022-03-28 11:25:57 -0400

    Add example and explaination to the subset script

--- APPS_subdataset/extract_files.sh
+++ APPS_subdataset/extract_files.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#Usage ./extract_files.sh <file containing paths of input file line by line>
+#It is expected that train folder from the APPS dataset is available in the current directory
 target="$1_dir"
 mkdir $target
 while IFS= read -r line; do 
