adca65d27e163bb997ae792ed374df36a4829c14
Derek <derekmpham@gmail.com>
2022-03-30 16:40:37 -0700

    Update 2long script and output file with questions that are too long

--- APPS_subdataset/get-2long-questions.py
+++ APPS_subdataset/get-2long-questions.py
@@ -13,8 +13,9 @@ d6 = './test/intro-questions.txt_dir'
 rootdirs = [d1, d2, d3, d4, d5, d6]
 # 2920 is roughly 4000 tokens
 word_lim = 2920 #1500
+char_lim = word_lim * 4.7 # average word is 4.7 characters
 
-def get2longqs(d, wl):
+def get2longqs(d, wl, cl):
 	for subdir, dirs, files in os.walk(d):
 		max_sol_wc = 0
 		for f in dirs:
@@ -26,12 +27,12 @@ def get2longqs(d, wl):
 			if os.path.exists(sf):
 				sc = open(sf)
 				sols = json.load(sc)
-				wcounts = [len(word_tokenize(s)) for s in sols if len(s) <= wl]
+				wcounts = [len(word_tokenize(s)) for s in sols if len(s) <= cl]
 				max_sol_wc = max(wcounts) if wcounts else wl
 
 			if q_nw + max_sol_wc >= wl:
 				print(os.path.join(subdir, f))
 
 for d in rootdirs:
-	get2longqs(d, word_lim)
+	get2longqs(d, word_lim, char_lim)
 	print()
