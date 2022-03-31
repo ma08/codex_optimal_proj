ceff64c8960ecb0cf7d6d582236a55ceb16aed36
gabriele-tombesi96 <gabrieletombesi4@gmail.com>
2022-03-31 13:53:14 -0400

    change davinci_1 folder name

--- davinci1_demo/script.py
+++ davinci1_demo/script.py
@@ -11,30 +11,15 @@ response = openai.Completion.create(
   top_p=1,
   frequency_penalty=0,
   presence_penalty=0,
-  # stop=stop,
   n=choices_number,
 )
 
 
 with open("resp.txt",'w',encoding = 'utf-8') as f:
-   # f.write("my first file\n")
    for i in range(0,choices_number):
-     # f.write("choice {}".format(i))
      resp=response.choices[i]
      f.write("{} ".format(resp))
      f.write("\n")
 
 f.close()
 
-  # responses = ["choice:" + choice.text for choice in response.choices]
-  # print(responses)
-
-# response = openai.Classification.create(
-#     file="file-jVRliPwwlOx1zYXi3H77vkzq",
-#     query="movie is very good",
-#     search_model="ada", 
-#     model="curie", 
-#     max_examples=3
-# )
-
-# print(response)
