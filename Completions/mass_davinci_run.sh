7e4aaa10e86cd8f531b97c773c8da755cc958e0d
Sourya Kakarla <sourya4@ladduu.com>
2022-04-12 11:08:30 -0400

    Add unbuffered option to edit module running

--- Completions/mass_davinci_run.sh
+++ Completions/mass_davinci_run.sh
@@ -5,20 +5,40 @@ function echo_ts
 {
 	echo $1 | ts
 }
+
 COUNTER=$(( 0 ))
 LIMIT=$(( 2640 ))
+CONTINUE_FROM=$(( 0 ))
 
-input_set=train/intro-questions.txt
-# for input_set in $test_or_train/*.txt; do 
+# input_set=train/intro-questions.txt
+input_set="$1"
+
+temp="$2"
+k="$3"
+sleep_time="$4"
 
 input_dir="$input_set"_dir
+echo_ts "Runnin completion API (first stage) for "
+echo_ts "input directory $input_dir"
+echo_ts "temperature  T1 $temp"
+echo_ts "k  K1 $k"
+
+# for input_set in $test_or_train/*.txt; do 
+
 # echo input_directory $input_dir
 
 for question in $input_dir/*/question.txt; do
-    echo_ts "completed: $COUNTER, next question: $question"
     arrIN=(${question//\// })
-    # echo question $question
     num=${arrIN[2]}                  #
+    echo_ts $num
+    num_question=$(expr $num+0)
+    echo_ts $num_question
+    num_question=$(expr $num+0)
+    if (( num_question <= CONTINUE_FROM )); then
+        continue 
+    fi
+    echo_ts "completed: $COUNTER, next question: $question"
+    # echo question $question
 
 
     out_dir=davinci_runs/$input_dir/$num
@@ -29,7 +49,7 @@ for question in $input_dir/*/question.txt; do
     # echo num $num
     # echo $question $out_dir $num
 
-    python3 run_davinci.py $question $out_dir 2>&1 
+    python3 run_davinci.py $question $out_dir $temp $k $sleep_time 2>&1 
     #python3 $out_dir/gen_code_out.py > $out_dir/out_of_gen_code.txt 2>&1 &
 
     (( COUNTER++ ))
